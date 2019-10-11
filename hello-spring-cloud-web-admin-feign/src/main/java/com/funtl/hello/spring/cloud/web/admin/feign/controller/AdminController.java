d3159a58cc636215e2e3567b511b17b6f32bf8f6
zcy166 <1663588662@qq.com>
2019-10-11 15:17:17 +0800

    feign

--- hello-spring-cloud-web-admin-feign/src/main/java/com/funtl/hello/spring/cloud/web/admin/feign/controller/AdminController.java
+++ hello-spring-cloud-web-admin-feign/src/main/java/com/funtl/hello/spring/cloud/web/admin/feign/controller/AdminController.java
@@ -1,6 +1,6 @@
-package com.funtl.hello.spring.cloud.web.admin.ribbor.controller;
+package com.funtl.hello.spring.cloud.web.admin.feign.controller;
 
-import com.funtl.hello.spring.cloud.web.admin.ribbor.service.AdminService;
+import com.funtl.hello.spring.cloud.web.admin.feign.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,11 +12,8 @@ public class AdminController {
     @Autowired
     private AdminService adminService;
 
-
-
     @RequestMapping(value = "hi", method = RequestMethod.GET)
-    public String sayHi(String message){
-        System.out.println("ad = ");
-        return adminService.sayHis(message);
+    public  String sayHi(String message){
+        return adminService.sayHi(message);
     }
 }
