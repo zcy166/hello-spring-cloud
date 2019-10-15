ade04587a9ad8d90ec570ee4ccbe1bf5c03893d8
zcy166 <1663588662@qq.com>
2019-10-15 11:15:02 +0800

    过滤

--- hello-spring-cloud-zuul/src/main/java/com/funtl/hello/spring/cloud/zuul/ZuulApplication.java
+++ hello-spring-cloud-zuul/src/main/java/com/funtl/hello/spring/cloud/zuul/ZuulApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+//开启zuul功能
 @EnableZuulProxy
+
 @EnableEurekaClient
 @SpringBootApplication
 public class ZuulApplication {
--- hello-spring-cloud-zuul/src/main/java/com/funtl/hello/spring/cloud/zuul/provide/WebAdminFallbackProvide.java
+++ hello-spring-cloud-zuul/src/main/java/com/funtl/hello/spring/cloud/zuul/provide/WebAdminFallbackProvide.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -13,6 +14,7 @@ import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
+@Component
 public class WebAdminFallbackProvide implements FallbackProvider {
 
     @Override
@@ -33,16 +35,19 @@ public class WebAdminFallbackProvide implements FallbackProvider {
 
             @Override
             public HttpStatus getStatusCode() throws IOException {
+                System.out.println(1);
                 return HttpStatus.OK;
             }
 
             @Override
             public int getRawStatusCode() throws IOException {
+                System.out.println(2);
                 return HttpStatus.OK.value();
             }
 
             @Override
             public String getStatusText() throws IOException {
+                System.out.println(3);
                 return HttpStatus.OK.getReasonPhrase();
             }
 
@@ -53,6 +58,7 @@ public class WebAdminFallbackProvide implements FallbackProvider {
 
             @Override
             public InputStream getBody() throws IOException {
+                System.out.println(4);
                 ObjectMapper objectMapper = new ObjectMapper();
                 Map<String, Object> map = new HashMap<>();
                 map.put("status", 200);
@@ -62,6 +68,7 @@ public class WebAdminFallbackProvide implements FallbackProvider {
 
             @Override
             public HttpHeaders getHeaders() {
+                System.out.println(5);
                 HttpHeaders headers = new HttpHeaders();
                 // 和 getBody 中的内容编码一致
                 headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
